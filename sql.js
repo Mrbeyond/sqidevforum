f0bdd48a4a29abe0e243fdf1a81c52b246abfff2
Mrbeyond <beyondbeyond602@gmail.com>
2021-05-25 10:23:31 +0100

    Implemented heroku clearDB config

--- sql.js
+++ sql.js
@@ -3,14 +3,28 @@ require('dotenv').config();
 
 module.exports = {
   sql : ()=>{
-    const con = mysql.createConnection(
-      {
-        host: process.env.host,
-        user: process.env.user,
-        password: process.env.password,
-        database: process.env.db
-      }
-    )
+    /** Manual live configuration */
+    // const con = mysql.createConnection(
+    //   {
+    //     host: process.env.host,
+    //     user: process.env.user,
+    //     password: process.env.password,
+    //     database: process.env.db
+    //   }
+    // )
+
+    /** Manual local configuration */
+    // const con = mysql.createConnection(
+    //   {
+    //     host: process.env.host_local,
+    //     user: process.env.user_local,
+    //     password: process.env.password_local,
+    //     database: process.env.db_local
+    //   }
+    // )
+    
+    /** String live configuration */
+    const con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
     con.connect();
 
     return con;
